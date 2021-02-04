69b04614d1f8a2ace38bd88031eca37c12acbf20
Bartosz Skiba <bartski816@student.polsl.pl>
2021-02-04 20:52:12 +0100

    Added some fancy stuff

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
-    const COORDINATES = {lon: 360, lat:114}; //span on the map (longtitude 360 degrees, latitude 114 degrees)
-    const INITIAL_COORDINATES ={lon: 180,longLetter: 'W', lat:54,latLetter: 'N'}; 
+    const SPAN= {lat: 114, lon: 360}
+    const BOUNDERIES= {min:{lat: 57,lon:-180},max:{lat: -57, lon: 180}};
     var map = L.map('map',{
+        zoomSnap: 0,
+        zoomDelta: 0.25,
+        wheelPxPerZoomLevel: 80,
         crs: L.CRS.Simple,
-        center: [1249,3224],
-        minZoom: 3
+        center: [SPAN.lat/2,SPAN.lon/2],
+        minZoom: 2,
+        maxZoom: 5
     });
-    let bounds = [[57,-180],[-57,180]];
+    let bounds = [[BOUNDERIES.min.lat,BOUNDERIES.min.lon],[BOUNDERIES.max.lat,BOUNDERIES.max.lon]]; //set bounds for imageOverlay
     const imageUrl = "res/image--000.png";
     let image = L.imageOverlay(imageUrl,bounds).addTo(map);
-    map.fitBounds(bounds);
+    map.fitBounds(bounds); //show map
+    map.setZoom(3);
 });
