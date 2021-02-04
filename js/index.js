b31b7544d7e9240f0d318953f2d1d256738bf8a0
Bartosz Skiba <bartski816@student.polsl.pl>
2021-02-04 20:27:00 +0100

    created a skeleton for future work

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    const COORDINATES = {lon: 360, lat:114} //span on the map (longtitude 360 degrees, latitude 114 degrees) 
+    const COORDINATES = {lon: 360, lat:114}; //span on the map (longtitude 360 degrees, latitude 114 degrees)
+    const INITIAL_COORDINATES ={lon: 180,longLetter: 'W', lat:54,latLetter: 'N'}; 
+    var map = L.map('map',{
+        crs: L.CRS.Simple,
+        center: [1249,3224],
+        minZoom: 3
+    });
+    let bounds = [[57,-180],[-57,180]];
+    const imageUrl = "res/image--000.png";
+    let image = L.imageOverlay(imageUrl,bounds).addTo(map);
+    map.fitBounds(bounds);
 });
