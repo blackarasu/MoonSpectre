9aae8f70af5cc7dd8d6825d0857b317297f0c336
Bartosz Skiba <bartski816@student.polsl.pl>
2021-02-04 21:46:46 +0100

    Map bounds and some user friendly interactions

--- js/index.js
+++ js/index.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
     const SPAN= {lat: 114, lon: 360}
+    /*
+     *  +lat => north 
+     *  -lat => south
+     *  +lon => east
+     *  -lon => west
+    */
     const BOUNDERIES= {min:{lat: 57,lon:-180},max:{lat: -57, lon: 180}};
+    let bounds = L.latLngBounds([[BOUNDERIES.min.lat,BOUNDERIES.min.lon],[BOUNDERIES.max.lat,BOUNDERIES.max.lon]]); //set bounds for imageOverlay
+    const imageUrl = "res/image--000.png";
     var map = L.map('map',{
-        zoomSnap: 0,
-        zoomDelta: 0.25,
+        maxBounds: bounds,
+        maxBoundsViscosity: 2/3,
         wheelPxPerZoomLevel: 80,
         crs: L.CRS.Simple,
         center: [SPAN.lat/2,SPAN.lon/2],
+        zoom: 3,
+        zoomSnap: 0,
+        zoomDelta: 0.25,
         minZoom: 2,
         maxZoom: 5
     });
-    let bounds = [[BOUNDERIES.min.lat,BOUNDERIES.min.lon],[BOUNDERIES.max.lat,BOUNDERIES.max.lon]]; //set bounds for imageOverlay
-    const imageUrl = "res/image--000.png";
     let image = L.imageOverlay(imageUrl,bounds).addTo(map);
     map.fitBounds(bounds); //show map
-    map.setZoom(3);
 });
