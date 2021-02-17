4f7b867051bb448432a6cf3dcb6ffef7346c9d86
Bartosz Skiba <bartski816@student.polsl.pl>
2021-02-17 22:51:01 +0100

    +geojson loader +icon loader #refactored code

--- js/index.js
+++ js/index.js
@@ -1,26 +1,53 @@
-$(document).ready(function(){
-    const SPAN= {lat: 114, lon: 360}
+$(document).ready(function () {
+    let map = initializeMap(); //show map
+    let control = initializeLoader(map);//show loader on map
+});
+function initializeLoader(map) {
+    L.Control.FileLayerLoad.LABEL = '<img class="icon" src="svg/folder.svg" alt="file icon"/>';
+    control = L.Control.fileLayerLoad({
+        fitBounds: true,
+        layerOptions: {
+            pointToLayer: function (data, latlng) {
+                return L.marker(latlng);
+            },
+            onEachFeature: onEachFeature
+        }
+    });
+    control.addTo(map);
+    return control;
+}
+
+function initializeMap() {
+    const SPAN = { lat: 114, lon: 360 };
     /*
-     *  +lat => north 
+     *  +lat => north
      *  -lat => south
      *  +lon => east
      *  -lon => west
     */
-    const BOUNDERIES= {min:{lat: 57,lon:-180},max:{lat: -57, lon: 180}};
-    let bounds = L.latLngBounds([[BOUNDERIES.min.lat,BOUNDERIES.min.lon],[BOUNDERIES.max.lat,BOUNDERIES.max.lon]]); //set bounds for imageOverlay
+    const BOUNDERIES = { min: { lat: 57, lon: -180 }, max: { lat: -57, lon: 180 } };
+    let bounds = L.latLngBounds([[BOUNDERIES.min.lat, BOUNDERIES.min.lon], [BOUNDERIES.max.lat, BOUNDERIES.max.lon]]); //set bounds for imageOverlay
     const imageUrl = "res/image--000.png";
-    var map = L.map('map',{
+    let map = L.map('map', {
         maxBounds: bounds,
-        maxBoundsViscosity: 2/3,
+        maxBoundsViscosity: 2 / 3,
         wheelPxPerZoomLevel: 80,
         crs: L.CRS.Simple,
-        center: [SPAN.lat/2,SPAN.lon/2],
+        center: [SPAN.lat / 2, SPAN.lon / 2],
         zoom: 3,
         zoomSnap: 0,
         zoomDelta: 0.25,
         minZoom: 2,
         maxZoom: 5
     });
-    let image = L.imageOverlay(imageUrl,bounds).addTo(map);
-    map.fitBounds(bounds); //show map
-});
+    L.imageOverlay(imageUrl, bounds).addTo(map);
+    map.fitBounds(bounds);
+    return map;
+}
+function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.name && feature.properties.diameter && feature.properties.height && feature.properties["name origin"] && feature.properties.terrainType) {
+        let prop = feature.properties;
+        layer.bindPopup(`${prop.terrainType} ${prop.name}</br>${prop["name origin"]}.</br>Height: ${prop.height}</br>Diameter: ${prop.diameter}`);
+    }
+}
+
