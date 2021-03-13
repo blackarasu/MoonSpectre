40375d1dcfe824c38d85ffc5c7fc0dcbb9dda87d
Bartosz Skiba <bartski816@student.polsl.pl>
2021-03-14 00:21:50 +0100

    ER Distinct features and clean map moved all libraries to local +function which add to map only those features which are distinct (need to have same coordinates and properties in lower case) +function which clean map

--- js/index.js
+++ js/index.js
@@ -1,33 +1,113 @@
-$(document).ready(function () {
-    let map = initializeMap(); //show map
-    let control = initializeLoader(map);//show loader on map
-});
+const SPAN = { lat: 114, lon: 360 };
+/*
+    *  +lat => north
+    *  -lat => south
+    *  +lon => east
+    *  -lon => west
+*/
+var layers = new Array();
+var features = new Array();
+const BOUNDERIES = { min: { lat: 57, lon: -180 }, max: { lat: -57, lon: 180 } };
+(function (window) {
+    window.addEventListener('load', function () {
+        let map = initializeMap(); //show map
+        let control = initializeLoader(map);//show loader on map
+    });
+}(window));
+
 function initializeLoader(map) {
+    var markers = new Array();
     L.Control.FileLayerLoad.LABEL = '<img class="icon" src="img/folder.svg" alt="file icon"/>';
-    control = L.Control.fileLayerLoad({
+    let control = L.Control.fileLayerLoad({
         fitBounds: true,
         layerOptions: {
-            pointToLayer: function (data, latlng) {
-                return L.marker(latlng);
+            pointToLayer: function (feature, latlng) {
+                var marker = L.marker(latlng);
+                addToMarkers(marker, generateHash(feature));
+                return marker;
             },
             onEachFeature: onEachFeature
         }
+    }).addTo(map);
+    control.loader.on('data:loaded', function (event) {
+        if (markers.length > 0) {
+            for(const key in event.layer._layers){
+                if(event.layer._layers[`${key}`]._popupHandlersAdded == undefined){
+                    event.layer._layers[`${key}`].removeFrom(event.layer);
+                }
+              }
+            layers.push(new Object({ layer: event.layer, name: event.filename }));
+        }
+        else {
+            event.layer.remove();//no markers has been added to layer so layer is useless
+        }
+        markers = new Array();//clear for another load file
     });
-    control.addTo(map);
+    control.loader.on('data:error', function (error) {
+        console.error(`${error.error.fileName}:${error.error.lineNumber} - ${error.error.message} - Please send an error message and scrreenshot to the developer via link in the footer.`);
+    });
     return control;
+
+    function generateHash(feature) {
+        let prop = feature.properties;
+        let hash = `${feature.geometry.coordinates.toString()} 
+                    ${prop.terrainType.toString().toLowerCase()} 
+                    ${prop.name.toString().toLowerCase()} 
+                    ${prop["name origin"].toString().toLowerCase()} 
+                    ${prop.height.toString().toLowerCase()} 
+                    ${prop.diameter.toString().toLowerCase()}`;
+        return hash.hashCode();
+    }
+
+    function addToMarkers(marker, hash) {
+        markers.push(new Object({ marker: marker, hash: hash }));
+    }
+
+    function onEachFeature(feature, layer) {
+        let [isAdded, hash] = addToFeatures(feature);
+        if (isAdded == true) {
+            let prop = feature.properties;
+            if (prop && prop.name && prop.diameter && prop.height && prop["name origin"] && prop.terrainType) {
+                layer.bindPopup(`[${feature.geometry.coordinates.toString()}]</br>
+                                ${prop.terrainType} 
+                                ${prop.name}</br>${prop["name origin"]}.</br>Height: 
+                                ${prop.height}</br>Diameter: ${prop.diameter}`);
+            }
+        }
+        else {
+            let isRemoved = removeMarker(hash);
+            if (isRemoved == false) {
+                alert(`Feature already exists but marker: ${hash} could not be removed! Please send an error message and scrreenshot to the developer via link in the footer.`);
+            }
+        }
+    }
+
+    function addToFeatures(feature) {//returns true if feature was added succesfully otherwise function returns false
+        let hash = generateHash(feature);
+        for (let i = 0; i < features.length; i++) {
+            if (features[i].hash == hash) {
+                return [false, hash];
+            }
+        }
+        features.push(new Object({ feature: feature, hash: hash }));
+        return [true, hash];
+    }
+
+    function removeMarker(hash) {
+        for (let i = 0; i < markers.length; i++) {
+            if (markers[i].hash == hash) {
+                markers[i].marker.remove();
+                markers.shift(i);
+                return true;
+            }
+        }
+        return false; //could NOT remove Marker for some reason
+    }
 }
 
 function initializeMap() {
-    const SPAN = { lat: 114, lon: 360 };
-    /*
-     *  +lat => north
-     *  -lat => south
-     *  +lon => east
-     *  -lon => west
-    */
-    const BOUNDERIES = { min: { lat: 57, lon: -180 }, max: { lat: -57, lon: 180 } };
     let bounds = L.latLngBounds([[BOUNDERIES.min.lat, BOUNDERIES.min.lon], [BOUNDERIES.max.lat, BOUNDERIES.max.lon]]); //set bounds for imageOverlay
-    const imageUrl = "img/image--000.png";
+    const IMAGE_URL = "img/image--000.png";
     let map = L.map('map', {
         maxBounds: bounds,
         maxBoundsViscosity: 2 / 3,
@@ -40,14 +120,20 @@ function initializeMap() {
         minZoom: 2,
         maxZoom: 6
     });
-    L.imageOverlay(imageUrl, bounds).addTo(map);
+    L.imageOverlay(IMAGE_URL, bounds).addTo(map);
     map.fitBounds(bounds);
     return map;
 }
-function onEachFeature(feature, layer) {
-    if (feature.properties && feature.properties.name && feature.properties.diameter && feature.properties.height && feature.properties["name origin"] && feature.properties.terrainType) {
-        let prop = feature.properties;
-        layer.bindPopup(`${prop.terrainType} ${prop.name}</br>${prop["name origin"]}.</br>Height: ${prop.height}</br>Diameter: ${prop.diameter}`);
-    }
+
+function clearMap() {
+    clearLocalStorage();
+    layers.forEach(layer => {
+        layer.layer.remove();
+    });
+    layers = new Array();
+    features = new Array();
 }
 
+function clearLocalStorage() {
+    localStorage.clear();
+}
